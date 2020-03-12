ca984d5ffabdcea1b5d88a738e31bafb1ef42712
Devin Dias <devindias.tech@gmail.com>
2020-03-12 08:32:34 -0500

    completed remove handler
    nested find by inside of remove

--- src/posts/posts-handler.ts
+++ src/posts/posts-handler.ts
@@ -59,14 +59,11 @@ export const getPostsHandler = (_req: Request, res: Response) => {
 }
 
 export const getPostHandler = (req: Request, res: Response) => {
-    
     const { params } = req;
                          //parse INT ?????
     const id: number = parseInt(params.id);
     /// omg typse script is AMZIng i did ctrl clcik and realized i was missing id
     findById(id) // dont understand why this is erroring out
-
-    
     .then((post: post) => {
         if(post){
             const response = postResponseBuilder(post)
@@ -75,6 +72,34 @@ export const getPostHandler = (req: Request, res: Response) => {
         else{
             const err: Error = {
                 name: "no post",
+                message: "no post inside of the Database with this ID"   
+            }
+            const response = errorResponseBuilder(err)
+            return res.status(200).send(response)
+        }
+    })
+    .catch( (err: Error) => {
+        const error = errorResponseBuilder(err)
+        return res.status(500).send(error)
+    })
+}
+
+export const removePostHandler = (req: Request, res: Response) => {
+    const { params } = req;
+    const id: number = parseInt(params.id);
+    //the reason I doubled nested these was to give a more acurate error if the was trying to delete something that deosnt exist
+    findById(id) 
+    .then((post: post) => {
+        if(post){
+          remove(id)
+          .then((id: number) => {
+            const response = removePostResponseBuilder(id)
+            return res.send(response)
+          })
+        }
+        else{
+            const err: Error = {
+                name: "no post with this iD",
                 message: "no post inside of the Database with this ID"   
             }
             const response = errorResponseBuilder(err)
